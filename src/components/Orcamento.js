57e0aec64e0d66dab335af5d3fb88a79fa8d979f
Tales Lopes <taleslopesramos@gmail.com>
2019-04-17 18:41:59 -0300

    Style Orcamento component

--- src/components/Orcamento.js
+++ src/components/Orcamento.js
@@ -1,19 +1,46 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 
 const Orcamento = (props) => {
     return(
-        <View>
-            <View>
-                <Text>{props.data.seller}</Text>
-                <Text>{props.data.customer}</Text>
+        <View style={styles.container}>
+            <View style={styles.firstView}>
+                <Text style={styles.normalText}>Vendedor:</Text>
+                <Text style={styles.normalText}>Comprador:</Text>
+            </View>
+            <View style={styles.secondView}>
+                <Text style={styles.normalText}>{props.data.seller}</Text>
+                <Text style={styles.normalText}>{props.data.customer}</Text>
             </View>
             <View>
-                <Text>{props.data.value}</Text>
+                <Text style={styles.valorText}>{props.data.value}</Text>
             </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection:"row",
+        padding:10,
+        borderBottomWidth: 0.3,
+        alignItems:'center',
+    },
+    firstView:{
+        marginRight:10
+    },
+    secondView:{
+        marginRight:'auto'
+    },
+    normalText: {
+      fontSize: 16,
+    },
+    valorText: {
+        fontWeight: 'bold',
+        fontSize: 17,
+          color: 'green',
+    },
+});
 
 export default Orcamento;
