f8c6e43861a4dd22da8ed57e1610f87e54837c17
Tales Lopes <taleslopesramos@gmail.com>
2019-04-17 16:42:25 -0300

    Style header and status bar

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,6 @@
+import { StatusBar } from 'react-native'
+
+// Deixa a status bar da mesma cor do header no Android
+StatusBar.setBackgroundColor('#2196F3');
+// Deixa a cor da status bar branca no IOS
+StatusBar.setBarStyle('light-content');
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,14 @@ import Main from './pages/main'
 
 const rootStack = createStackNavigator({
     Main,
+},
+{
+    defaultNavigationOptions:{
+        headerStyle:{
+            backgroundColor:"#2196F3"
+        },
+        headerTintColor:"#FFF",
+    }
 });
 
 const App  = createAppContainer(rootStack);
